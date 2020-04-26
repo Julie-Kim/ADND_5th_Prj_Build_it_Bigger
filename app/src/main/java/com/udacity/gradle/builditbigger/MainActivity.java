0c6ed351964a37dbc50a7b262f9dd50207735199
Julie Kim <juliekim8312@gmail.com>
2020-04-26 22:52:59 +0900

    ### **Step 1: Create a Java library**
    Your first task is to create a Java library that provides jokes. Create a new Gradle Java project either using the Android Studio wizard, or by hand. Then introduce a project dependency between your app and the new Java Library. If you need review, check out demo 4.01 from the course code.
    
    Make the button display a toast showing a joke retrieved from your Java joke telling library.

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,5 +1,6 @@
 package com.udacity.gradle.builditbigger;
 
+import android.example.com.javajokes.Joker;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -40,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, new Joker().getJoke(), Toast.LENGTH_SHORT).show();
     }
 
 
---
+++ javaJokes/src/main/java/android/example/com/javajokes/Joker.java
@@ -0,0 +1,8 @@
+package android.example.com.javajokes;
+
+public class Joker {
+
+    public String getJoke() {
+        return "Coffee is the most important meal of the day.";
+    }
+}
